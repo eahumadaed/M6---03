8b56a85f820602679e7c1e793c44ec1cd5c67f26
eahumaded <eahumada@lag.cl>
2024-05-24 13:07:17 -0400

    Init

---
+++ mi_proyecto/settings.py
@@ -0,0 +1,64 @@
+from pathlib import Path
+import os
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'holaMundo',
+]
+ROOT_URLCONF = 'mi_proyecto.urls'
+WSGI_APPLICATION = 'mi_proyecto.wsgi.application'
+SECRET_KEY = 'Mi_super_clave_segura_1234'
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#Parametros que uso normalmente en los proyectos
+
+ALLOWED_HOSTS = []
+DEBUG = True
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+LANGUAGE_CODE = 'es-cl'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
+
